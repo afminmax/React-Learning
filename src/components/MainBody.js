350b82551f8d650e13fe0b68c9148de7a6a521ad
afminmax <allen.frasier@aquinix.com>
2020-06-10 17:33:19 +0200

    mainbody minor update

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function Mainbody() {
   return (
-    <div>The first eight Star Wars movies in sequence are quite awesome!</div>
+    <div>
+      The first eight Star Wars movies in sequence are quite awesome! The last
+      three suck.
+    </div>
   );
 }
