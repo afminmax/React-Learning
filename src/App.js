e6b9ed41049804723eb78ee904381065ab3f5779
afminmax <allen.frasier@aquinix.com>
2020-06-10 12:38:52 +0200

    removed all default templates - site working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import Header from './components/Header';
 import MainBody from './components/MainBody';
 import Footer from './components/Footer';
